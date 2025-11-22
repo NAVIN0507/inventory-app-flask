cd0e84c862c54e1b648a7713bd73d186f1b254ce
NAVIN0507 <navinofficial2005@gmail.com>
2025-11-22 16:18:47 +0530

    updated frontend

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://inventory-app-flask.onrender.com/api",
+    baseURL: process.env.BACKEND_URL! || "http://127.0.0.1:5000/api",
     withCredentials:true,
 })
