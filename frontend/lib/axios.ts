c6311b28ff3c2d33804cfbdb1a38ce6118f604cc
NAVIN0507 <navinofficial2005@gmail.com>
2025-11-22 16:40:58 +0530

    remove api

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: process.env.BACKEND_URL! || "http://127.0.0.1:5000/api",
+    baseURL: process.env.BACKEND_URL!,
     withCredentials:true,
 })
